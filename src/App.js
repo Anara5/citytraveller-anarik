1d5285ecc437e1dc2e6777728fc65788757d5089
Anara Zhunusova <anarikrulit@gmail.com>
2022-07-06 16:09:16 +0200

    a basic start for my app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import CityTravel from './components/CityTravel';
 
 function App() {
   return (
-    <div className="App">
-      <CityTravel />
+    <div className='App'>
+      <div className="cityTravel">
+        <h1>City Travel</h1>
+      </div>
+      <div id='earth'>
+        <CityTravel />
+      </div>
     </div>
   );
 }
--- src/components/CityTravel.jsx
+++ src/components/CityTravel.jsx
@@ -1,44 +1,10 @@
 import React from 'react';
-import { useState, useEffect } from "react";
-import { db } from '../firebase/firebase';
-// import { uid } from 'react-uid';
-import Card from './Card';
-import MyGlobe from './MyGlobe';
-
 
 function CityTravel() {
-
-    const [cities, setCities] = useState([]);
-
-    useEffect(() => {
-        db.collection('cities').onSnapshot(snapshot => {
-            setCities(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
-        });
-    }, []);
-
-    
 
     return (
         <>
             <div>
-                <h1>City Travel</h1>
-            </div>
-            <div>
-                <MyGlobe />
-            </div>
-            <div>
-
-
-
-                {cities.map(city => (
-                    <Card
-                        // key={uid(city)}
-                        key={city.id}
-                        image={city.image}
-                        title={city.title}
-                        text={city.text}
-                    />
-                ))}
             </div>
         </>
     );
