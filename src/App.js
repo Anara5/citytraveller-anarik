34463f97ec17998a071fc7833fe53a9e801713e1
Anara Zhunusova <anarikrulit@gmail.com>
2022-07-07 02:27:52 +0200

    THREE-globe
    changed the globe into THREE-object and added mouse animation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,96 @@
 import './App.css';
 import CityTravel from './components/CityTravel';
+import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
+import atmosphereVertexShader from './shaders/atmosphereVertex.glsl';
+import atmosphereFragmentShader from './shaders/atmosphereFragment.glsl';
+import React from 'react';
+import earthImg from './background/earthmap1k.jpg';
+import gsap from 'gsap';
+
 
 function App() {
   return (
-    <div className='App'>
-      <div className="cityTravel">
-        <h1>City Travel</h1>
-      </div>
-      <div id='earth'>
-        <CityTravel />
-      </div>
-    </div>
+    <>
+      <CityTravel />
+    </>
   );
 }
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer(
+  { antialias: true, alpha: true }
+);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+document.body.appendChild(renderer.domElement);
+
+const sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(earthImg),
+  }),
+  new THREE.ShaderMaterial({
+    vertexShader,
+    fragmentShader,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load(earthImg)
+      }
+    }
+  })
+);
+
+scene.add(sphere);
+
+const atmosphere = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 50, 50),
+  new THREE.ShaderMaterial({
+    vertexShader: atmosphereVertexShader,
+    fragmentShader: atmosphereFragmentShader,
+    blending: THREE.AdditiveBlending,
+    side: THREE.BackSide,
+    uniforms: {
+      globeTexture: {
+        value: new THREE.TextureLoader().load(earthImg)
+      }
+    }
+  })
+);
+
+atmosphere.scale.set(1.1, 1.1, 1.1);
+
+scene.add(atmosphere);
+camera.position.z = 15;
+
+const group = new THREE.Group();
+group.add(sphere);
+scene.add(group);
+
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  sphere.rotation.y += 0.003;
+  group.rotation.y += 0.003;
+  gsap.to(group.rotation, {
+    x: -mouse.y * 0.3,
+    y: mouse.x * 0.5,
+    duration: 2,
+  })
+}
+animate();
+
+window.self.addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
 
 export default App;
--- src/components/CityTravel.jsx
+++ src/components/CityTravel.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+// import Card from './Card';
 
 function CityTravel() {
 
+    // const myId = 'LoK8jf8eL0McR2Sf5bwLj3bUWU89-s6KgjKdSRge0nQ';
+    // const endpoint = `https://api.unsplash.som/photos/random/?client_id=${myId}`;
+
+    // const [image] = React.useState(null);
+
+    //     fetch(endpoint)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             console.log(data);
+    //         });
+
+
+   
+
     return (
         <>
-            <div>
+            <div className="cityTravel">
+            <h1>City Travel</h1>
+                {/* <Card image={image} title="City" text="This is a city" /> */}
+
             </div>
         </>
     );
